4a7ec1ca1c6b4b508278e21e810fe0ffd1d54066
ZitroneMelone <57600331+ZitroneMelone@users.noreply.github.com>
2023-10-13 15:41:16 +0200

    Add files via upload

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -19,23 +20,40 @@ namespace WpfApp1
     public partial class MainWindow : Window
     {
         private bool isPlayerX = true;
-        private char[,] gameboard;
+        private char[,] gameboard = new char[3,3];
+       
 
-
-
-        public MainWindow()
-        {
+        public MainWindow(){
             InitializeComponent();
         }
 
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
+        private void Button_Click(object sender, RoutedEventArgs e){
             if (sender is Button button)
             {
                 if (string.IsNullOrEmpty(button.Content as string))
                 {
                     button.Content = isPlayerX ? "X" : "O";
+                    UpdateGameboard(button.Tag.ToString());
                     isPlayerX = !isPlayerX;
+                }
+            }
+        }
+        private void UpdateGameboard(String code){
+            string[] parts = code.Split(',');
+            int x = int.Parse(parts[0]);
+            int y = int.Parse(parts[1]);
+            Console.WriteLine(parts[0] + " " + parts[1]);
+            gameboard[x, y] = isPlayerX ? 'X' : 'O';
+        }
+
+        private void CheckWin(){
+            for (int i = 0; i < gameboard.Length; i++)
+            {
+                if (gameboard[i, 0].Equals(gameboard[i, 1]) && gameboard[i, 1].Equals(gameboard[i, 2]))
+                {
+                    if(!gameboard[i, 0].Equals(null)) { 
+                    Console.WriteLine("Winner is " + gameboard[i, 0]);
+                    }
                 }
             }
         }
